8b9bf2e80b1236829f0b248f0a1acade9fee4f79
DuncanSamson <duncancsamson@gmail.com>
2025-03-16 19:16:25 +0000

    updating publishing

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,5 +3,6 @@
 @section('body')
 <div class="p-8">
     <h1 class="text-3xl font-bold">Hello world!</h1>
+    <p>wewrdtfyuihgfcxdrsetyuighjcfxdsaew56475rytughfkdxhsyw45364r5ytughfkgjdfhsy</p>
 </div>
 @endsection
