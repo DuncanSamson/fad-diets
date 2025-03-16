a629cecee31551c9670687e8cebdc947421a403b
DuncanSamson <duncancsamson@gmail.com>
2025-03-16 19:39:44 +0000

    publishing testing

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,5 +4,7 @@
 <div class="p-8">
     <h1 class="text-3xl font-bold">Hello world!</h1>
     <p>wewrdtfyuihgfcxdrsetyuighjcfxdsaew56475rytughfkdxhsyw45364r5ytughfkgjdfhsy</p>
+    <p>meh</p>
+    <h2>Hello world</h2>
 </div>
 @endsection
