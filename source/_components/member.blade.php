d9d39159bd06b647824c46e6ccb87806966063d7
Duncan Samson <duncancsamson@gmail.com>
2025-04-29 23:11:08 +0100

    HotFix

--- source/_components/member.blade.php
+++ source/_components/member.blade.php
@@ -3,13 +3,13 @@
     <img src="{{$image}} " alt="" class="size-10 rounded-full bg-gray-50 ">
 
     <div class="text-sm/6">
-        <p class="font-semibold text-gray-900 text-lg">
+        <p class="font-semibold text-gray-900 text-xs">
             <a href="#">
                 <span class="absolute inset-0"></span>
                 {{$fullname}}
             </a>
         </p>
-        <p class="text-gray-600 font-bold text-9xl flex items-end ">{{$jobtitle}}</p>
+        <p class="text-gray-600 font-bold text-xl flex items-end ">{{$jobtitle}}</p>
         <p class="text-gray-600 text-xs flex items-end ">{{$individualBlurb}}</p>
     </div>
 </div>
