a4b5d0fea1872e25fdd73c59a1c1d0eb94c30ce2
Duncan Samson <duncancsamson@gmail.com>
2025-05-02 21:00:10 +0100

    Adding sienna plugin (WCAG)

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -13,6 +13,7 @@
         <title>{{ $page->title }}</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+        <script src="https://website-widgets.pages.dev/dist/sienna.min.js" defer></script>
     </head>
     <body class="bg-emerald-800 text-emerald-50 font-sans antialiased">
         <nav class="flex bg-emerald-500 shadow-lg shadow-emerald-500 shadow-emerald-600 bg-gradient-to-b from-emerald-300 from-5% to-emerald-600 to-95%">
