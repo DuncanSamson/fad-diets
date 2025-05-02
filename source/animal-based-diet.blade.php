9eb3ec2cdc410d9142bd9a19193f8ab8574c056b
Duncan Samson <duncancsamson@gmail.com>
2025-05-02 20:22:09 +0100

    changing h3 to h2 (WCAG)

--- source/animal-based-diet.blade.php
+++ source/animal-based-diet.blade.php
@@ -6,16 +6,16 @@ image: animal-based-diet.jpg
 @extends('_layouts.main')
 @section('content')
     <h1 class="text-7xl font-bold text-center mb-6 pb-3 mb-10 border-b border-b-emerald-200" >Animal Based Diet – The Paul Saladino Plan </h1>
-    <h3 class="text-xl pt-8 pb-2 font-bold">Who Invented It?</h3>
+    <h2 class="text-xl pt-8 pb-2 font-bold">Who Invented It?</h3>
     <p class="text-lg">Popularized by Dr Paul Saladino, who originally was an advocate of the carnivore diet before evolving his approach to include fruit and honey. </p>
-    <h3 class="text-xl pt-8 pb-2 font-bold">What Affects Could It Have On Your Brain?</h3>
+    <h2 class="text-xl pt-8 pb-2 font-bold">What Affects Could It Have On Your Brain?</h3>
     <p class="text-lg">Followers report enhanced clarity, sharper focus, and a more stable mood. These effects may be due to the elimination of many common dietary toxins and refined carbohydrates.</p>
-    <h3 class="text-xl pt-8 pb-2 font-bold">What Affects Could It Have On Your Body?</h3>
+    <h2 class="text-xl pt-8 pb-2 font-bold">What Affects Could It Have On Your Body?</h3>
     <p  class="text-lg">Followers claim benefits including increased energy, improved digestion, reduced autoimmune symptoms, and better muscle development.</p>
-    <h3 class="text-xl pt-8 pb-2 font-bold">Fun Facts</h3>
+    <h2 class="text-xl pt-8 pb-2 font-bold">Fun Facts</h3>
     <p class="text-lg">Dr Paul Saladino was at one point in time a major advocate of the carnivore diet and wrote the book called the Carnivore code.
         Plant based consumption is on the increase in the UK with 1.2 million people adopting a vegan diet in the last 2 years, likely leading to increased consumption of seed oils and Oxalates.</p>
-    <h3 class="text-xl pt-8 pb-2 font-bold">Sources</h3>
+    <h2 class="text-xl pt-8 pb-2 font-bold">Sources</h3>
     <ul class="list-disc pl-8">
         <li>
             <x-link href="https://www.paulsaladinomd.co/animal-based-diet">Animal-Based Diet – Paul Saladino, MD</x-link>
--- source/fad-diet.blade.php
+++ source/fad-diet.blade.php
@@ -10,7 +10,7 @@ image: fad-diets-logo-light-green.png
         <section class="text-lg mb-6 flex gap-4 pb-10 border-b border-emerald-200">
             <img class="w-48" src="/assets/images/animal-based-diet.jpg">
             <div>
-                <h3 class="font-bold text-xl pb-8">Animal Based Diet – The Paul Saladino Plan</h3>
+                <h2 class="font-bold text-xl pb-8">Animal Based Diet – The Paul Saladino Plan</h3>
                 <p class="text-lg mb-6">
                     This diet focuses on eating primarily animal products such as meat, eggs, and dairy, while also including fruit and honey, with extra focus on avoiding seed oils and vegetables particularl those high in Oxalates (A vegetable’s defence chemical).
                 </p>
@@ -22,7 +22,7 @@ image: fad-diets-logo-light-green.png
 
         <section class="text-lg mb-6 flex gap-4 pb-10 border-b border-emerald-200">
             <div>
-                <h3 class="font-bold text-xl pb-8">Carnivore Diet – All Animal, No Plants</h3>
+                <h2 class="font-bold text-xl pb-8">Carnivore Diet – All Animal, No Plants</h3>
                 <p class="text-lg mb-6">
                     This diet excludes all plant-based foods, relying exclusively on animal products, such as meat, fish, eggs, and dairy to provide all the nutrients the body and mind requires. There are variations to the diet with some users eliminating the dairy element from the diet.
                 </p>
@@ -35,7 +35,7 @@ image: fad-diets-logo-light-green.png
         <section class="text-lg mb-6 flex gap-4">
             <img class="w-48" src="/assets/images/Lion-diet.jpg">
             <div>
-                <h3 class="font-bold text-xl pb-8">The Lion Diet</h3>
+                <h2 class="font-bold text-xl pb-8">The Lion Diet</h3>
                 <p class="text-lg mb-6">
                     The Lion Diet includes only three things: ruminant meat, salt, and water. No spices, oils, or caffeine.
                     It’s often followed for 30–90 days before slowly reintroducing foods to test reactions.
