f5dea19aa315f67577da3a03a27110f1903cea6e
Duncan Samson <duncancsamson@gmail.com>
2025-05-02 20:41:30 +0100

    changing backing on button(WCAG)

--- source/_components/button.blade.php
+++ source/_components/button.blade.php
@@ -1,3 +1,3 @@
-<a href="{{$page}}" class="inline-block bg-green-600 text-white px-6 py-3 rounded-md hover:bg-green-700 transition">
+<a href="{{$page}}" class="inline-block bg-green-700 text-white px-6 py-3 rounded-md hover:bg-green-700 transition">
     {{$slot}}
 </a>
