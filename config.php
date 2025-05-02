81ff18378f96103f8080d40af22fd64938db3016
Duncan Samson <duncancsamson@gmail.com>
2025-05-02 07:51:29 +0100

    update meta data: adding log

--- config.php
+++ config.php
@@ -5,5 +5,6 @@ return [
     'baseUrl' => '',
     'title' => 'faddiets.uk',
     'description' => 'A website about Fad Diets and a 8bit game about health eating.',
+    'image' => '/assets/images/fad-diets-logo-light-green.png',
     'collections' => [],
 ];
