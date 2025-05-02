681455d12728ce01894a7f7639326f20af74a471
Duncan Samson <duncancsamson@gmail.com>
2025-05-02 19:17:44 +0100

    meta data update

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,9 +6,10 @@
         <link rel="canonical" href="{{ $page->getUrl() }}">
         <meta name="description" content="{{ $page->description }}">
         <meta property="og:title" content="{{ $page->title }}"/>
+        <meta name="og:description" content="{{ $page->description }}">
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://faddiets.uk{{ $page->getUrl() }}"/>
-        <meta property="og:image" content="https://faddiets.uk/assets/images/{{$page->image}} "/>
+        <meta property="og:image" content="https://faddiets.uk/assets/images/{{$page->image}}"/>
         <title>{{ $page->title }}</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
