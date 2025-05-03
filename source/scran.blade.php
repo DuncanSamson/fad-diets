9124db0547680237748d0d0ca1df03f66b8a9f6e
Duncan Samson <duncancsamson@gmail.com>
2025-05-03 19:52:34 +0100

    updated: layouts for mobile version of website for scran page

--- source/scran.blade.php
+++ source/scran.blade.php
@@ -7,10 +7,12 @@ image: fad-diets-logo-light-green.png
 @section('content')
     <article class="max-w-3xl mx-auto px-4 py-10">
         <h1 class="text-7xl font-bold text-center mb-6 pb-3 mb-10 border-b border-b-emerald-200">What’s in Your Scran?</h1>
-        <section class="text-lg mb-6 flex gap-4 pb-10 border-b border-emerald-200">
-            <img class="w-48" alt="Big Mac" src="/assets/images/big-mac.jpg">
-            <div>
-                <h2 class="font-bold text-xl pb-8">Canola Oil Aka Canadian Oil Low Acid</h2>
+        <section class="text-lg mb-6 block md:flex gap-4 pb-10 border-b border-emerald-200">
+            <div class="flex justify-center md:justify-start flex-auto md:flex-1">
+                <img class="w-48" alt="Big Mac" src="/assets/images/big-mac.jpg">
+            </div>
+            <div class="flex-1">
+                <h2 class="text-center md:text-left text-xl font-bold pb-8">Canola Oil Aka Canadian Oil Low Acid</h2>
                 <p class="text-lg mb-6">Oil that is extracted from genetically modified rapeseeds from the rapeseed plant.</p>
                 <x-button page="/scran-canola-oil">
                     Read More About It
@@ -18,9 +20,9 @@ image: fad-diets-logo-light-green.png
             </div>
         </section>
 
-        <section class="text-lg mb-6 flex gap-4 pb-10 border-b border-emerald-200">
-            <div>
-                <h2 class="font-bold text-xl pb-8">Palm Fat Aka Palm Oil Solid Fraction</h2>
+        <section class="text-lg mb-6 block md:flex gap-4 pb-10 border-b border-emerald-200">
+            <div class="flex-1">
+                <h2 class="text-center md:text-left font-bold text-xl pb-8">Palm Fat Aka Palm Oil Solid Fraction</h2>
                 <p class="text-lg mb-6">
                     Fat extracted from the fruit of the oil palm tree, used widely in processed foods and cosmetics.
                 </p>
@@ -28,13 +30,17 @@ image: fad-diets-logo-light-green.png
                     Read More About It
                 </x-button>
             </div>
-            <img class="w-48" alt="mars bar" src="/assets/images/mars-bar.jpg">
+            <div class="flex justify-center md:justify-start flex-auto md:flex-1 pt-4 md:pt-0">
+                <img class="w-48" alt="mars bar" src="/assets/images/mars-bar.jpg">
+            </div>
         </section>
         
-        <section class="text-lg mb-6 flex gap-4">
-            <img class="w-48" alt="Irn bru can" src="/assets/images/irnbru.jpg">
-            <div>
-                <h2 class="font-bold text-xl pb-8">Sunset Yellow FCF (E110)</h2>
+        <section class="text-lg mb-6 block md:flex gap-4">
+            <div class="flex justify-center md:justify-end flex-auto md:flex-1">
+                <img class="w-48" alt="Irn bru can" src="/assets/images/irnbru.jpg">
+            </div>
+            <div class="flex-1">
+                <h2 class="text-center md:text-left font-bold text-xl pb-8">Sunset Yellow FCF (E110)</h2>
                 <p class="text-lg mb-6">
                     A bright orange artificial dye made from petroleum, used to give foods and drinks a strong orange
                     colour.
