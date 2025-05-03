221143d55b96677699306fc6fd73f22d1187c2d2
Duncan Samson <duncancsamson@gmail.com>
2025-05-03 20:03:22 +0100

    fix the logo

--- source/_components/navbar.blade.php
+++ source/_components/navbar.blade.php
@@ -1,9 +1,9 @@
 <header class="absolute inset-x-0 top-0 z-50">
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
-            <a href="#" class="-m-1.5 p-1.5">
-                <span class="sr-only">Your Company</span>
-                <img class="h-10 invert w-auto" src="/assets/images/FAD-Diets.svg" alt="">
+            <a href="/" class="-m-1.5 p-1.5">
+                <span class="sr-only">FAD Diets</span>
+                <img class="h-10 invert w-auto" src="/assets/images/FAD-Diets.svg" alt="logo">
             </a>
         </div>
         <div class="flex flex-1 justify-end lg:hidden">
