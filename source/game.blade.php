0817506487d02667677f62111db419798f20fca4
Duncan Samson <duncancsamson@gmail.com>
2025-05-06 19:27:22 +0100

    Game added

--- source/game.blade.php
+++ source/game.blade.php
@@ -5,5 +5,11 @@ image: fad-diets-logo-light-green.png
 ---
 @extends("_layouts.main")
 @section("content")
-    <h1>about</h1>
+    <div class="text-7xl font-bold text-center hidden md:block mb-6 pt-8 pb-3 border-b border-b-emerald-200">
+        <h1>Game</h1>
+    </div>
+
+    <x-game>
+
+    </x-game>
 @endsection
