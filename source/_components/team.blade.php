9ba6ca09d792220c4c36f2e413ceca73c8d6c138
DuncanSamson <duncancsamson@gmail.com>
2025-05-06 10:10:07 +0100

    home page fix

--- source/_components/team.blade.php
+++ source/_components/team.blade.php
@@ -2,7 +2,7 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl lg:mx-0">
             <h2 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Our team</h2>
-            <p class="mt-6 text-lg/8 text-gray-600">we're a passionate collective of students from New College Lanarkshire, united by a shared commitment to promoting healthier lifestyles. Our mission is to deliver accurate, engaging, and accessible information that empowers individuals to make informed dietary choices.
+            <p class="mt-6 text-lg/8 text-gray-600">We're a passionate collective of students from New College Lanarkshire, united by a shared commitment to promoting healthier lifestyles. Our mission is to deliver accurate, engaging, and accessible information that empowers individuals to make informed dietary choices.
             </p>
         </div>
         <ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
