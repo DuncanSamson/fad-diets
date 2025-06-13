c83257a7c9574013db131a7f276200788a214bec
Duncan Samson <15039074+DuncanSamson@users.noreply.github.com>
2025-06-13 11:18:07 +0100

    gdpr compliance

--- source/index.blade.php
+++ source/index.blade.php
@@ -15,9 +15,6 @@ author: Duncan Samson
                   I am the lead Game developer behind our game's functionality, bringing its mechanics to life,
                   and the artistic genius responsible for the sprite artwork that gives it its unique visual charm."></x-member>
 
-        <x-member fullname="" image="/assets/images/clown.jpg" jobtitle=""
-                  individualBlurb=""></x-member>
-
         <x-member fullname="Duncan Samson" image="/assets/images/100_0039.JPG" jobtitle="Web Developer"
                   individualBlurb="Hi, I’m Duncan, and I’m currently a student at New College Lanarkshire. My responsibilities include overseeing the web development process,
                   ensuring everything runs smoothly, and coordinating with the team to deliver the best possible user experience.
