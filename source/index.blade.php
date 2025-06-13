92077ffff7ca8a7981495441c61ab7d14ad44373
Duncan Samson <15039074+DuncanSamson@users.noreply.github.com>
2025-06-13 11:04:33 +0100

    gdpr compliance

--- source/index.blade.php
+++ source/index.blade.php
@@ -15,8 +15,8 @@ author: Duncan Samson
                   I am the lead Game developer behind our game's functionality, bringing its mechanics to life,
                   and the artistic genius responsible for the sprite artwork that gives it its unique visual charm."></x-member>
 
-        <x-member fullname="Howard the Clown" image="/assets/images/clown.jpg" jobtitle="Class Clown"
-                  individualBlurb="Hey! I’m Howie The Clown — official intern, snack hoarder, and the reason this site nearly missed every deadline. I was supposed to do research, but I mostly just used ChatGPT prompts like 'what is food' and spent three hours arguing with a toaster. I didn’t really write anything useful, but I did suggest we use more glitter (they said no). So if you find something helpful on this site, it definitely wasn’t me. But if you find a typo or a weird fact about cheese... yeah, that one’s mine. You’re welcome."></x-member>
+        <x-member fullname="" image="/assets/images/clown.jpg" jobtitle="Class Clown"
+                  individualBlurb=""></x-member>
 
         <x-member fullname="Duncan Samson" image="/assets/images/100_0039.JPG" jobtitle="Web Developer"
                   individualBlurb="Hi, I’m Duncan, and I’m currently a student at New College Lanarkshire. My responsibilities include overseeing the web development process,
